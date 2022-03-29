4520368942367984e0d32ffd0290f5ec2aa06c06
prithvi <kumarakshay895@gmail.com>
2022-03-29 22:09:11 +0530

    initial setup

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import ProductListing from "./pages/ProductListing";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/products-listing" element={<ProductListing />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
